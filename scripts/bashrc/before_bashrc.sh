4e3778f7205dd5c04aa45ad8b568348f16af1a67
Willie Owens <willie.owens@urjanet.com>
2019-01-16 09:53:31 -0500

    Go updates, additional gradle aliases, dev cd function

--- scripts/bashrc/before_bashrc.sh
+++ scripts/bashrc/before_bashrc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export PATH=~/Development/Scripts/dev:~/Development/Scripts/urjanet:/opt/eclipse:/opt/liquibase:/opt/packer:/opt/casperjs/bin:/opt/phantomjs/bin:/opt/slimerjs:/opt/firefox:/opt/cassandra/bin:/opt/intellij/bin:~/.local/bin:$PATH
+export PATH=~/Development/Scripts/dev:~/Development/Scripts/urjanet:/opt/eclipse:/opt/liquibase:/opt/packer:/opt/casperjs/bin:/opt/phantomjs/bin:/opt/slimerjs:/opt/firefox:/opt/cassandra/bin:/opt/intellij/bin:~/.local/bin:/usr/lib/go-1.10/bin:$PATH
+
+export GOPATH=~/Development/go
+export PATH=$PATH:$GOPATH/bin
 
 # Custom bash prompt for Git: https://github.com/magicmonty/bash-git-prompt
 GIT_PROMPT_ONLY_IN_REPO=1
@@ -8,13 +11,17 @@ source ~/Development/Tools/bash-git-prompt/gitprompt.sh
 
 export AWS_REGION=us-east-1
 
-alias dev="cd ~/Development"
+dev() {
+    cd ~/Development/"$1"
+}
+#alias dev="cd ~/Development"
 alias scr="cd ~/Development/Scripts"
 alias scr-b="cd ~/Development/Scripts/bashrc"
 
 # Gradle Aliases
 alias gwb="gw bootRun"
-alias gwt="gw build integrationTest"
+alias gwt="gw clean build integrationTest"
+alias gwd="gw -Dtest.debug"
 
 # Git Aliases
 alias s="git status"
@@ -34,6 +41,7 @@ alias aa="git add -u"
 alias co="git checkout"
 alias b="git branch"
 alias merge="git merge"
+alias stash="git stash"
 
 # Shortcut to get the current AWS_PROFILE
 getaws() {
@@ -50,20 +58,27 @@ chaws() {
   unset AWS_SECRET_ACCESS_KEY
 
   case $1 in
+    d*    ) unset AWS_PROFILE;;
     p*    ) export AWS_PROFILE=prod;;
     d*|i* ) export AWS_PROFILE=dev;;
+    r*    ) export AWS_PROFILE=reinvent;;
+    e*    ) export AWS_PROFILE=eksworkshop;;
     *     ) echo "[FAIL] Usage: chaws <env>"
             echo "  where <env> is in: p[roduction] i[ntegration]|d[evelopment]"
             return;
   esac
 
   if [ -z "$2" ]; then
-    echo "Updated AWS profile to $AWS_PROFILE"
+    if [ -z "$AWS_PROFILE" ]; then
+        echo "Unset AWS profile - using DEFAULT"
+    else
+        echo "Updated AWS profile to $AWS_PROFILE"
+    fi
   fi
 }
 
 # Default to dev AWS creds
-chaws i true
+chaws d true
 
 ftxt() {
   find . -type f -exec grep "$@" {} /dev/null \;
